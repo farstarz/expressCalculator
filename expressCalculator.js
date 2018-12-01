828c8537f9e3d01f2013f84703bee5f9992cb600
Pandey <vaibhav.pandey@wmeng.com>
2018-12-01 11:21:19 -0800

    initial commit of final code

---
+++ expressCalculator.js
@@ -0,0 +1,62 @@
+// Dependencies
+var express = require("express");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Create express app instance.
+var app = express();
+
+// Routes
+// What routes do you need to have? Which ones are optional?
+// TODO Add your routes here
+app.get("/:operation/:operand1/:operand2", function(req, res) {
+
+  // TODO parse out the variables from the request
+  // Parameters are received from the URL
+  let operation = req.params.operation;
+  let operand1 = req.params.operand1;
+  let operand2 = req.params.operand2;
+  // TODO make sure they're converted to integers (and not strings)
+  // Parameters are converted to integers
+  operand1 = parseInt(operand1);
+  operand2 = parseInt(operand2);
+
+  // Initialize the result variable to send later
+  var result;
+  // Switch statement chooses operation based on the operation parameter.
+  switch (operation) {
+  // BONUS - How could you use * + etc. inside the app.get()?
+  case "add":
+    result = operand1 + operand2;
+    // Add your logic here. Pun intended.
+    break;
+  case "subtract":
+    result = operand1 - operand2;
+    // Subtract logic
+    break;
+  case "multiply":
+    result = operand1 * operand2;
+    // Multiply
+    break;
+  case "divide":
+    result = operand1/operand2;
+    // Divide
+    break;
+  default:
+    // Handle anything that isn't specified
+    result =
+        "Sorry! The only valid operations are add, subtract, multiply, and divide.";
+  }
+
+  // We return the result back to the user in the form of a string
+  res.send(result.toString());
+
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
